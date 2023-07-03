0e331e5f31f96f8fa441ad02a26d688ab50ab9cb
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-03 22:25:39 +0400

    Update Address.sol

--- Address.sol
+++ Address.sol
@@ -1,2 +1,18 @@
 // A 20-byte Ethereum address. The address object has many helpful member functions, the main ones being balance 
 // ( returns the account balance) and transfer (transfers ether to the account).
+
+
+pragma solidity ^0.8.0;
+
+contract AddressExample {
+    function getBalance(address _account) external view returns (uint256) {
+        return _account.balance;
+    }
+
+    function transferEther(address payable _recipient, uint256 _amount) external {
+        require(address(this).balance >= _amount, "Insufficient balance in the contract");
+
+        _recipient.transfer(_amount);
+    }
+}
+
