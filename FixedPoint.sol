36f368ee8270670f92c080730eda64fb007126ec
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-03 11:52:24 +0400

    Update FixedPoint.sol

--- FixedPoint.sol
+++ FixedPoint.sol
@@ -1,4 +1,4 @@
-// Fixed Point( fixed, ufixed)
+ // Fixed Point( fixed, ufixed)
  // Fixed-point numbers, declared with (u)fixedMxN where M is the size in bits (increments of 8 up to 256) and N is the number of decimals after the point ( up to 18);
  // e.g unfixed32x2
 
