ea0d90c7ad05f989cedecdbfd0e1f860a84020a1
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-07-03 11:40:02 +0400

    integer

---
+++ Integer.sol
@@ -0,0 +1,3 @@
+// Integer (int, uint)
+// Signed (int) and unsigned (uint) integers, declared in increments of 8 bits from int8 to uint 256.
+// Without a size suffix, 256-bit quantities are used, to match the word size of the EVM
