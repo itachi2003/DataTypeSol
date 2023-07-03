36b37f7ed20d79bcd3d7b8f645a965d8c12d4738
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-07-03 11:40:51 +0400

    add info about Signed integers

--- Integer.sol
+++ Integer.sol
@@ -1,3 +1,12 @@
 // Integer (int, uint)
 // Signed (int) and unsigned (uint) integers, declared in increments of 8 bits from int8 to uint 256.
 // Without a size suffix, 256-bit quantities are used, to match the word size of the EVM
+
+Signed Integers (int):
+
+int8: Represents a signed integer with a size of 8 bits (-128 to 127).
+int16: Represents a signed integer with a size of 16 bits (-32,768 to 32,767).
+int32: Represents a signed integer with a size of 32 bits (-2,147,483,648 to 2,147,483,647).
+int64: Represents a signed integer with a size of 64 bits (-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807).
+int128: Represents a signed integer with a size of 128 bits.
+int256: Represents a signed integer with a size of 256 bits.
