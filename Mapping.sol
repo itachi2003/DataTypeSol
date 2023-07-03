b9403439921ffb9d08d30205de2ca79a4d556a01
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-03 23:20:19 +0400

    Update Mapping.sol

--- Mapping.sol
+++ Mapping.sol
@@ -1,1 +1,19 @@
+// MAPPING
+// Hash lookup tables for key => value pairs : mapping(KEY_TYPE => VALUE_TYPE) NAME.
+
+
+pragma solidity ^0.8.0;
+
+contract MappingExample {
+    mapping(uint256 => string) public names;
+
+    function setName(uint256 _id, string memory _name) external {
+        names[_id] = _name;
+    }
+
+    function getName(uint256 _id) external view returns (string memory) {
+        return names[_id];
+    }
+}
+
 
