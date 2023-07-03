8009e8cebe13a3383bfcaadc89dd981831294c4c
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-03 22:38:35 +0400

    Update ByteArray.sol

--- ByteArray.sol
+++ ByteArray.sol
@@ -1,1 +1,14 @@
+// Byte array ( fixed )
+Fixed-size arrays of bytes, declared with bytes1 up to bytes32
+ 
+pragma solidity ^0.8.0;
 
+contract ByteArrayExample {
+    function manipulateByteArray() external pure returns (bytes2) {
+        bytes2 myBytes;
+        myBytes[0] = 0x41; // Assign hexadecimal value 'A' to the first element
+        myBytes[1] = 0x42; // Assign hexadecimal value 'B' to the second element
+
+        return myBytes;
+    }
+}
