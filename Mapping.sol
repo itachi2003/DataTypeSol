8d6213e75bf4e0d3a4e18b15b0bd54ed70bac142
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-03 23:21:30 +0400

    Update Mapping.sol

--- Mapping.sol
+++ Mapping.sol
@@ -17,3 +17,12 @@ contract MappingExample {
 }
 
 
+// The MappingExample contract allows you to associate names with unique IDs using a mapping.
+
+// The names mapping is like a table that associates uint256 IDs with corresponding string names.
+
+// The setName function lets you set the name for a specific ID. It takes two inputs: the ID (_id) and the name (_name). It updates the names mapping by assigning the given _name to the provided _id.
+
+// The getName function allows you to retrieve the name associated with a specific ID. It takes an input parameter _id and returns the corresponding name from the names mapping.
+
+
