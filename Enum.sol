203f001304530e848ca7058df99f257b4c231acc
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-07-03 23:02:34 +0400

    Update Enum.sol

--- Enum.sol
+++ Enum.sol
@@ -1,1 +1,22 @@
+// Enum
+
+// User-defined type for enumerating discrete values: enum NAME {LABEL1, LABEL2,...}
+
+
+pragma solidity ^0.8.0;
+
+contract EnumExample {
+    enum Status {Inactive, Active, Suspended, Deleted}
+
+    Status public userStatus;
+
+    function setUserStatus(uint256 _status) external {
+        require(_status < 4, "Invalid status value");
+        userStatus = Status(_status);
+    }
+
+    function getUserStatus() external view returns (Status) {
+        return userStatus;
+    }
+}
 
