c28382914c857be4bf9ecc83e02d7a2fd57118fc
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-07-03 22:05:15 +0400

    address

---
+++ Address.sol
@@ -0,0 +1,2 @@
+// A 20-byte Ethereum address. The address object has many helpful member functions, the main ones being balance 
+// ( returns the account balance) and transfer (transfers ether to the account)
